f337dcb5d5caf9eb406d4126cd1a59c4f46c4d74
Emanuel Rosu <emanuelr@microsoft.com>
2015-10-15 20:25:54 -0700

    initial commit

---
+++ giraffe-farm/Assets/Scripts/SwingHandler.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingHandler : MonoBehaviour {
+
+	private enum SwingState {
+		Unstarted,
+		Started,
+		Ended
+	}
+
+	private SwingState m_currentSwingState;
+
+	// Use this for initialization
+	void Start () {
+		m_currentSwingState = SwingState.Unstarted;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_currentSwingState = SetCurrentSwingState ();
+
+		if (m_currentSwingState == SwingState.Started) {
+			
+		} else {
+
+		}
+	}
+
+	private SwingState SetCurrentSwingState () {
+		if (m_currentSwingState == SwingState.Unstarted) {
+			if (Input.GetMouseButtonDown(0)) {
+				return SwingState.Started;
+			}
+		} else if (m_currentSwingState == SwingState.Started) {
+			if (Input.GetMouseButtonUp(0)) {
+				return SwingState.Unstarted;
+			}
+		}
+
+		return SwingState.Unstarted;
+	}
+}
