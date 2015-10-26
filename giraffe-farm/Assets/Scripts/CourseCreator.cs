cc6082f4998532c1786b2cec1bcaf78e717b1404
Emanuel Rosu <rosu.emanuel@gmail.com>
2015-10-25 20:18:03 -0700

    added logic for right facing blocks

--- giraffe-farm/Assets/Scripts/CourseCreator.cs
+++ giraffe-farm/Assets/Scripts/CourseCreator.cs
@@ -6,6 +6,7 @@ public class CourseCreator : MonoBehaviour {
 
     public List<GameObject> m_blockTypesForward = new List<GameObject>();
     public List<GameObject> m_blockTypesLeft = new List<GameObject>();
+    public List<GameObject> m_blockTypesRight = new List<GameObject>();
     public List<GameObject> m_course = new List<GameObject>();
     private Quaternion m_currRotation = Quaternion.identity;
 
@@ -30,13 +31,16 @@ public class CourseCreator : MonoBehaviour {
             } else if (lastBlock.name.Contains("forward"))
             {
                 m_currentDirection = Direction.Forward;
+            } else if (lastBlock.name.Contains("right"))
+            {
+                m_currentDirection = Direction.Right;
             }
 
             if (m_currentDirection == Direction.Forward)
             {
                 Renderer renderer = lastBlock.GetComponentInChildren<Renderer>();
                 Vector3 nextPos = Vector3.zero;
-                if (lastBlock.name.Contains("corner"))
+                if (lastBlock.name.Contains("corner") && !lastBlock.name.Contains("right"))
                 {
                     nextPos = new Vector3(renderer.transform.position.x, renderer.transform.position.y, renderer.transform.position.z + (4 * renderer.bounds.extents.z));
                 }
@@ -54,15 +58,25 @@ public class CourseCreator : MonoBehaviour {
                 {
                     placed.transform.position = new Vector3(placed.transform.position.x + (2 * renderer.bounds.extents.x), placed.transform.position.y, placed.transform.position.z - (2 * renderer.bounds.extents.x));
                 }
-            }
-
-          
-            
-            /*if (m_course[m_course.Count - 1].name.Contains("Round"))
+            } else if (m_currentDirection == Direction.Right)
             {
-                m_course[m_course.Count - 1].transform.rotation = (Quaternion.Euler(0.0f, 270.0f, 0.0f));
-                m_course[m_course.Count - 1].transform.position = nextPos;
-            }*/
+                Renderer renderer = lastBlock.GetComponentInChildren<Renderer>();
+                Vector3 nextPos = Vector3.zero;
+                if (lastBlock.name.Contains("corner"))
+                {
+                    nextPos = new Vector3(renderer.transform.position.x + (4 * renderer.bounds.extents.x), renderer.transform.position.y, renderer.transform.position.z);
+                }
+                else
+                {
+                    nextPos = new Vector3(renderer.transform.position.x + (2 * renderer.bounds.extents.x), renderer.transform.position.y, renderer.transform.position.z);
+                }
+                m_course.Add(GameObject.Instantiate(m_blockTypesRight[Random.Range(0, m_blockTypesRight.Count)], nextPos, Quaternion.identity) as GameObject);
+                GameObject placed = m_course[m_course.Count - 1];
+                if (placed.name.Contains("corner"))
+                {
+                    placed.transform.position = new Vector3(placed.transform.position.x + (2 * renderer.bounds.extents.x), placed.transform.position.y, placed.transform.position.z - (2 * renderer.bounds.extents.x));
+                }
+            }
         }
 
     }
