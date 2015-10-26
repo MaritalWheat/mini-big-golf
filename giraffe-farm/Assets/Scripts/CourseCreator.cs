2f3ad5a9416c9c4003709db818f79285d44ee2f0
Emanuel Rosu <rosu.emanuel@gmail.com>
2015-10-25 19:54:29 -0700

    course generation work

---
+++ giraffe-farm/Assets/Scripts/CourseCreator.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CourseCreator : MonoBehaviour {
+
+    public List<GameObject> m_blockTypesForward = new List<GameObject>();
+    public List<GameObject> m_blockTypesLeft = new List<GameObject>();
+    public List<GameObject> m_course = new List<GameObject>();
+    private Quaternion m_currRotation = Quaternion.identity;
+
+    private enum Direction
+    {
+        Forward,
+        Backward,
+        Left,
+        Right
+    }
+
+    private Direction m_currentDirection = Direction.Forward;
+    
+	void Start () {
+        m_course.Add(GameObject.Instantiate(m_blockTypesForward[0], Vector3.zero, Quaternion.identity) as GameObject);
+        for (int i = 0; i < 100; i++)
+        {
+            GameObject lastBlock = m_course[m_course.Count - 1];
+            if (lastBlock.name.Contains("left"))
+            {
+                m_currentDirection = Direction.Left;
+            } else if (lastBlock.name.Contains("forward"))
+            {
+                m_currentDirection = Direction.Forward;
+            }
+
+            if (m_currentDirection == Direction.Forward)
+            {
+                Renderer renderer = lastBlock.GetComponentInChildren<Renderer>();
+                Vector3 nextPos = Vector3.zero;
+                if (lastBlock.name.Contains("corner"))
+                {
+                    nextPos = new Vector3(renderer.transform.position.x, renderer.transform.position.y, renderer.transform.position.z + (4 * renderer.bounds.extents.z));
+                }
+                else
+                {
+                    nextPos = new Vector3(renderer.transform.position.x, renderer.transform.position.y, renderer.transform.position.z + (2 * renderer.bounds.extents.z));
+                }
+                m_course.Add(GameObject.Instantiate(m_blockTypesForward[Random.Range(1, m_blockTypesForward.Count)], nextPos, Quaternion.identity) as GameObject);
+            } else if (m_currentDirection == Direction.Left) {
+                Renderer renderer = lastBlock.GetComponentInChildren<Renderer>();
+                Vector3 nextPos = new Vector3(renderer.transform.position.x - (2 * renderer.bounds.extents.x), renderer.transform.position.y, renderer.transform.position.z);
+                m_course.Add(GameObject.Instantiate(m_blockTypesLeft[Random.Range(0, m_blockTypesLeft.Count)], nextPos, Quaternion.identity) as GameObject);
+                GameObject placed = m_course[m_course.Count - 1];
+                if (placed.name.Contains("corner"))
+                {
+                    placed.transform.position = new Vector3(placed.transform.position.x + (2 * renderer.bounds.extents.x), placed.transform.position.y, placed.transform.position.z - (2 * renderer.bounds.extents.x));
+                }
+            }
+
+          
+            
+            /*if (m_course[m_course.Count - 1].name.Contains("Round"))
+            {
+                m_course[m_course.Count - 1].transform.rotation = (Quaternion.Euler(0.0f, 270.0f, 0.0f));
+                m_course[m_course.Count - 1].transform.position = nextPos;
+            }*/
+        }
+
+    }
+	
+	void Update () {
+	    
+	}
+}
--- giraffe-farm/Assets/Scripts/SwingHandler.cs
+++ giraffe-farm/Assets/Scripts/SwingHandler.cs
@@ -12,7 +12,10 @@ public class SwingHandler : MonoBehaviour {
 	}
 
 	private SwingState m_currentSwingState;
-    private float m_ballThrust;
+    private Vector3 m_swingInputStartPosition;
+    private Vector3 m_swingInputEndPosition;
+    private Vector3 m_swingInputDirection;
+    private float m_swingTime;
 
 	void Start () {
 		m_currentSwingState = SwingState.Unstarted;
@@ -21,40 +24,63 @@ public class SwingHandler : MonoBehaviour {
 	void Update () {
 		m_currentSwingState = SetCurrentSwingState ();
 
-        if (m_currentSwingState == SwingState.Ended) {
-            Debug.Log("Performing swing.");
-            m_ballRigidBody.AddForce(m_ballRigidBody.transform.forward * m_ballThrust);
-            m_currentSwingState = SwingState.Unstarted;
-		} else {
-
-		}
+        
 	}
 
+    void FixedUpdate()
+    {
+        if (m_currentSwingState == SwingState.Ended)
+        {
+            Debug.Log("Performing swing.");
+            /*float rotationRadians = Mathf.Atan(m_swingInputDirection.z / m_swingInputDirection.x);
+            float rotationDegrees = 0.0f;
+            if (m_swingInputDirection.x > 0.0f)
+            {
+                rotationDegrees = 90.0f - (Mathf.Rad2Deg * rotationRadians);
+            } else
+            {
+                rotationDegrees = -(90.0f + (Mathf.Rad2Deg * rotationRadians));
+            }
+            Debug.Log("Degrees: " + rotationDegrees);
+            Transform parentTransform = m_ballRigidBody.gameObject.GetComponentInParent<Transform>();
+            m_ballRigidBody.constraints = RigidbodyConstraints.FreezePosition;
+            parentTransform.Rotate(Vector3.up, rotationDegrees, Space.Self);
+            m_ballRigidBody.MoveRotation(Quaternion.Euler(new Vector3(rotationDegrees, 0.0f)));
+            m_ballRigidBody.WakeUp();*/
+            float velocity = Vector3.Distance(m_swingInputStartPosition, m_swingInputEndPosition) / m_swingTime;
+            Debug.Log("Acceleration: " + velocity);
+            m_ballRigidBody.AddForce(Vector3.Normalize(m_swingInputDirection) * velocity);
+            m_currentSwingState = SwingState.Unstarted;
+        }
+        else
+        {
+
+        }
+    }
+
 	private SwingState SetCurrentSwingState () {
-        Debug.Log("Force: " + m_ballThrust);
+        //Debug.Log("Force: " + m_ballThrust);
         if (m_currentSwingState == SwingState.Unstarted) {
             if (Input.GetMouseButton(0)) {
                 Debug.Log("Starting swing.");
+                m_swingInputStartPosition = Input.mousePosition;
+                m_swingTime = 0.0f;
                 return SwingState.Started;
             }
 		} else if (m_currentSwingState == SwingState.Started) {
 			if (Input.GetMouseButtonUp(0)) {
                 Debug.Log("Ending swing.");
+                m_swingInputEndPosition = Input.mousePosition;
+                Vector3 rawDirection = m_swingInputStartPosition - m_swingInputEndPosition;
+                m_swingInputDirection = new Vector3(-rawDirection.x, 0.0f, -rawDirection.y);
+                Debug.Log("Swing direction: " + m_swingInputDirection);
                 return SwingState.Ended;
 			} else {
-                m_ballThrust = m_ballThrust + (1000.0f * Time.deltaTime);
+                m_swingTime += Time.deltaTime;
                 return SwingState.Started;
             }
 		}
 
-        if (m_ballThrust > 0.0f)
-        {
-            m_ballThrust -= 1000.0f * Time.deltaTime;
-            if (m_ballThrust < 0.0f)
-            {
-                m_ballThrust = 0.0f;
-            }
-        }
 		return SwingState.Unstarted;
 	}
 }
