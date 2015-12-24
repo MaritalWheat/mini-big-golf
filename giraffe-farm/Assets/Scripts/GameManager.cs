660b87122a6acf1b2a0728d5f0af005c7cb84a76
Emanuel Rosu <rosu.emanuel@gmail.com>
2015-12-24 15:14:19 -0600

    discount "paused" time from total game time

--- giraffe-farm/Assets/Scripts/GameManager.cs
+++ giraffe-farm/Assets/Scripts/GameManager.cs
@@ -25,6 +25,8 @@ public class GameManager : MonoBehaviour {
 	private static GameManager Instance;
 	private GameState m_currentState;
 	private float m_gameStartTime;
+	private float m_gamePauseStartTime;
+	private float m_gamePauseTotalTime;
 
 	public static GameState CurrentGameState {
 		get { return Instance.m_currentState; }
@@ -48,25 +50,33 @@ public class GameManager : MonoBehaviour {
 	public void StartGame() {
 		m_currentState = GameState.Started;
 		m_gameStartTime = Time.time;
+		m_gamePauseTotalTime = 0.0f;
 		//should probably use an event here eventually, this will be unscaleable...
 		UIManager.StartGame ();
 	}
 
 	//called from UI Pause Button
 	public void PauseGame() {
+		m_currentState = GameState.Paused;
+		m_gamePauseStartTime = Time.time;
+	}
 
+	//called from UI Unpause Button
+	public void UnpauseGame() {
+		m_gamePauseTotalTime += Time.time - m_gamePauseStartTime;
 	}
 
 	public static void EndGame() {
 		Instance.m_currentState = GameState.Ended;
 
-		float timePlayed = Time.time - Instance.m_gameStartTime;
+		float timePlayed = (Time.time - Instance.m_gameStartTime) - Instance.m_gamePauseTotalTime;
 		UIManager.PostGame (new GameStats(timePlayed, PlayerManager.Hits));
 	}
 
 	public static void ResetGame() {
 		Instance.m_currentState = GameState.Started;
 		Instance.m_gameStartTime = Time.time;
+		Instance.m_gamePauseTotalTime = 0.0f;
 
 		UIManager.Reset ();
 	}
