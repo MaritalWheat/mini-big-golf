2979bb505b306b69ce9f2bd433b53353bddc8e36
Emanuel Rosu <rosu.emanuel@gmail.com>
2016-01-02 00:51:57 -0800

    style cleanup

--- giraffe-farm/Assets/Scripts/AbstractTargetFollower.cs
+++ giraffe-farm/Assets/Scripts/AbstractTargetFollower.cs
@@ -10,8 +10,7 @@ namespace UnityStandardAssets.Cameras
 
         protected Rigidbody targetRigidbody;
 
-        protected virtual void Start()
-        {
+        protected virtual void Start() {
             // if auto targeting is used, find the object tagged "Player"
             // any class inheriting from this should call base.Start() to perform this action!
             if (m_AutoTargetPlayer)
@@ -22,8 +21,7 @@ namespace UnityStandardAssets.Cameras
             
         }
 
-        public void ManualUpdate()
-        {
+        public void ManualUpdate() {
             if (m_AutoTargetPlayer && (m_Target == null || !m_Target.gameObject.activeSelf))
             {
                 FindAndTargetPlayer();
@@ -35,26 +33,20 @@ namespace UnityStandardAssets.Cameras
         protected abstract void FollowTarget(float deltaTime);
 
 
-        public void FindAndTargetPlayer()
-        {
+        public void FindAndTargetPlayer() {
             // auto target an object tagged player, if no target has been assigned
             var targetObj = GameObject.FindGameObjectWithTag("Player");
-            if (targetObj)
-            {
+            if (targetObj) {
                 SetTarget(targetObj.transform);
 				targetRigidbody = m_Target.GetComponent<Rigidbody>();
             }
         }
 
-
-        public virtual void SetTarget(Transform newTransform)
-        {
+        public virtual void SetTarget(Transform newTransform) {
             m_Target = newTransform;
         }
 
-
-        public Transform Target
-        {
+        public Transform Target {
             get { return m_Target; }
         }
     }
