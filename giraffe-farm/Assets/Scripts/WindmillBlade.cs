cffd12ce479b71a0fa2ab2fdc3d0a9cca9416e1f
Emanuel Rosu <rosu.emanuel@gmail.com>
2016-01-19 21:01:43 -0800

    Added windmill blade script, not rotating around local center though (issue #5)

---
+++ giraffe-farm/Assets/Scripts/WindmillBlade.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindmillBlade : MonoBehaviour {
+
+	private Transform m_transform;
+	[SerializeField] private float m_speed = 0.0f;
+
+	void Awake () {
+		if (m_transform == null) {
+			m_transform = this.gameObject.transform;
+		}
+	}
+
+	void Update () {
+		m_transform.Rotate (Vector3.forward, Time.deltaTime * m_speed, Space.World);
+	}
+}
