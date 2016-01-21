2ec2e1b436a29b7255723b748ef2df9500d15ea1
Emanuel Rosu <rosu.emanuel@gmail.com>
2016-01-20 19:40:48 -0800

    Empty gameobject as center point for rotation hack for windmill blades - they now turn correctly! (related to issue #5)

--- giraffe-farm/Assets/Scripts/WindmillBlade.cs
+++ giraffe-farm/Assets/Scripts/WindmillBlade.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class WindmillBlade : MonoBehaviour {
 
 	private Transform m_transform;
+
+	[SerializeField] private Transform m_targetTransform;
 	[SerializeField] private float m_speed = 0.0f;
 
 	void Awake () {
@@ -13,6 +15,6 @@ public class WindmillBlade : MonoBehaviour {
 	}
 
 	void Update () {
-		m_transform.Rotate (Vector3.forward, Time.deltaTime * m_speed, Space.World);
+		m_transform.RotateAround (m_targetTransform.position, m_targetTransform.forward, Time.deltaTime * m_speed);
 	}
 }
