8f5605cdebbb9657ac97d34a872ad1f2615b9e84
Emanuel Rosu <rosu.emanuel@gmail.com>
2016-03-01 23:02:36 -0800

    Setup a kinda hacky system for detecting when ball hits a wall and playing appropriate sounds - progress towards #3

---
+++ giraffe-farm/Assets/Scripts/WallHitDetectionHelper.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallHitDetectionHelper : MonoBehaviour {
+
+	private bool m_wallHit;
+	public AudioClip m_hitClip;
+
+	void FixedUpdate() {
+		if (PlayerManager.Ball != null) {
+			this.transform.position = new Vector3(PlayerManager.Ball.GetComponent<Rigidbody>().worldCenterOfMass.x, PlayerManager.Ball.GetComponent<Rigidbody>().worldCenterOfMass.y + 0.05f, 
+			                                      PlayerManager.Ball.GetComponent<Rigidbody>().worldCenterOfMass.z);
+		}
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.tag != "Player") {
+			Debug.Log (other.gameObject.name);
+			//lol late nite hack to avoid hole logic colliders
+			if (other.gameObject.name.Contains("Model")) {
+				AudioManager.PlaySoundAtObject (this.gameObject, m_hitClip);
+			}
+		}
+	}
+}
