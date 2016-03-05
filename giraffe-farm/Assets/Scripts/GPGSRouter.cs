5294226a1030489436445b714cf5ec9bd1d3a72e
Emanuel Rosu <rosu.emanuel@gmail.com>
2016-03-05 00:26:30 -0800

    Added Google Play Game Services plugin + got sign-in prototype working! - progress towards #41

---
+++ giraffe-farm/Assets/Scripts/GPGSRouter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+
+public class GPGSRouter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// authenticate user:
+		GooglePlayGames.PlayGamesPlatform.Activate();
+		Social.localUser.Authenticate((bool success) => {
+			//mWaitingForAuth = false;
+			if (success) {
+				Debug.Log("Welcome " + Social.localUser.userName);
+				string token = GooglePlayGames.PlayGamesPlatform.Instance.GetToken();
+				Debug.Log(token);
+			} else {
+				Debug.Log("Authentication failed.");
+			}
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
